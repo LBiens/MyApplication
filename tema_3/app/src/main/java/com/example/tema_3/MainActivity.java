5cdf22b4f556946733a6b32292be19109dd926d4
Laurent Biens <laurent.0723@gmail.com>
2022-09-30 18:58:21 +0200

    errores en logs

--- tema_3/app/src/main/java/com/example/tema_3/MainActivity.java
+++ tema_3/app/src/main/java/com/example/tema_3/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.tema_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        log.d( tag: "debug", msg: "Este es mi mensaje de debug");
+        log.i( tag: "informacion", msg: "Este es mi mensaje de información");
+        log.w( tag: "warning", msg: "Este es mi mensaje de warning");
+        log.e( tag: "error", msg: "Este es mi mensaje de error");
     }
 }
